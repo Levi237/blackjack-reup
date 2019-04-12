98d8f5e81827e0f96f410c9c2921c25e4ac7a855
Levi Winans <levieiko@gmail.com>
2019-04-11 20:46:08 -0700

    correct files saved

---
+++ js/app.js
@@ -0,0 +1,266 @@
+console.log("WELCOME TO BLACKJACK!  YOU HAVE 10 CHIPS TO START")
+console.log("GOOD LUCK!")
+//--> BLACK JACK GAME
+
+
+//-->   END GAME PSEUDOCODE
+//buttons need to be used only when necessary
+//intervalTimer needed to slow down card display
+//opening page?
+//restart option
+
+
+//----------------------> CLASS OBJECT
+class Player {
+    constructor(name){
+        this.name = name;
+        this.chips = 10;
+        this.hand = [];
+        this.round = 0;
+    }
+}
+const dealer = {
+    name: "Bob",
+    hand: [],
+}
+
+let player = new Player("Fauxman");  //--> edit out later into prompt?
+let deck = [];
+let shuffledDeck = [];
+let playerFinalScore = 0;
+let dealerFinalScore = 0;
+let dealerCall = [];
+let alert = document.querySelector("#alert")
+const getSum = (total, num) => {return total + num;}
+
+
+//---------------------->   BUTTONS
+const playButton = document.getElementById('play-button');
+const hitButton = document.getElementById('hit-button');
+// const splitButton = document.getElementById('split-button')  //--> change rule so dealer quits at 17
+// const doubleButton = document.getElementById('double-button')  //--> change rule so dealer quits at 17
+// const splitButton = document.getElementById('split-button')  //--> Double Down
+// const restartButton = document.getElementById('restart-button')  //--> Restart
+const stayButton = document.getElementById('stay-button');
+const buyButton = document.getElementById('buy-button');
+//---------------------->    START GAME EVENT LISTENER
+playButton.addEventListener('click', () => {
+    displayReset();
+    stayButton.removeAttribute('disabled');
+    alert.innerText = "";
+    if (player.chips !== 0) {
+        player.chips = player.chips - 1;
+        clearField();
+        showRound();
+        makeDeck();
+        shuffleCards();
+        dealHand();
+        displayDeal();
+        showMoney()
+    } else {
+        buyIn()
+        console.log("you are out of money, would you like to buy in for another 10?")
+        //RESET GAME     
+    }     
+})
+//---------------------->    HIT BUTTON
+hitButton.addEventListener('click', () => {
+    hitMe();
+    displayPlayerCard();
+    showMoney()
+})
+//---------------------->    STAY BUTTON
+stayButton.addEventListener('click', () => {
+    handValue();
+    dealerAI();
+    callHand();
+    showMoney();
+    stayButton.setAttribute('disabled', true);
+})
+//---------------------->    BUY IN BUTTON
+buyButton.addEventListener('click', () => {
+    player.chips += 10;
+    showMoney();
+    buyButton.style.visibility = "hidden";
+})
+//---------------------->    DEAL CARD VISUAL
+const displayDeal = () => {
+    let blankPlayer = document.querySelector("#blankPlayer");
+    let blankDealer = document.querySelector("#blankDealer");
+        setTimeout(function() {
+            blankPlayer.insertAdjacentHTML("beforebegin", `<element class="slide card ${player.hand[0].suit} r${player.hand[0].face}"></element>`)
+        },200);
+        setTimeout(function() {
+            blankDealer.insertAdjacentHTML("beforebegin", `<element id="dealerDown" class="slide card back-red"></element>`);
+        },400);
+        setTimeout(function() {
+            blankPlayer.insertAdjacentHTML("beforebegin", `<element class="slide card ${player.hand[1].suit} r${player.hand[1].face}"></element>`);
+        },800);
+        setTimeout(function() {
+            blankDealer.insertAdjacentHTML("beforebegin", `<element class="slide card ${dealer.hand[1].suit} r${dealer.hand[1].face}"></element>`);
+        },1000);
+        if (player.hand[0].value === 11 && player.hand[1].value === 10 || player.hand[1].value === 11 && player.hand[0].value === 10){
+            alert.innerText = "21 blackjack!";
+        }
+}
+//---------------------->    SHOW HIT ME CARD
+const displayPlayerCard = () => {
+    document.getElementById("blankPlayer").insertAdjacentHTML("beforebegin", `<element class="slide card ${player.hand[0].suit} r${player.hand[0].face}"></element>`);
+}
+const displayDealerCard = () => {
+    // setTimeout(function() {
+    document.getElementById("blankDealer").insertAdjacentHTML("beforebegin", `<element class="slide card ${dealer.hand[0].suit} r${dealer.hand[0].face}"></element>`);
+    // },400);  
+}
+//---------------------->    RESET CARD DISPLAY
+const displayReset = () => {
+    let childPlayer = document.getElementById("player-hand");
+        childPlayer.innerHTML = '';
+    let ep = document.createElement("element");
+        ep.setAttribute("id","blankPlayer")
+        childPlayer.appendChild(ep);
+    let childDealer = document.getElementById("dealer-hand");
+        childDealer.innerHTML = '';
+    let ed = document.createElement("element");
+        ed.setAttribute("id","blankDealer");
+        childDealer.appendChild(ed);
+}
+//---------------------->   DISPLAY WINDOW
+const showMoney = () => {
+    let money = player.chips * 10;
+    document.getElementById("chips").innerText = money;
+}
+const showRound = () => {
+    let counter = player.round;
+    document.getElementById("round").innerText = counter;
+}
+//---------------------->    CLEAR FIELD
+const clearField = () => {
+    player.hand = [];
+    dealer.hand = [];
+    deck = [];
+    shuffledDeck = [];
+    player.round++;
+    dealerCall = [];
+}
+//---------------------->    BUILD A DECK
+const makeDeck = () => {
+    // const cardWord = "card";
+    const suits = ["spades", "hearts", "diamonds", "clubs"];
+    const face = ["Ace", "02", "03", "04", "05", "06", "07", "08", "09", "10", "Jack", "Queen", "King"];
+    for (let s = 0; s < suits.length; s++){
+        for (let f = 0; f < face.length; f++){
+            let card = {name: face[f] + " of " + suits[s], face: face[f], suit: suits[s], value: +face[f]}
+            if (card.face ===  "Ace"){
+                card.value = 11
+            } else if (card.face === "Jack" || card.face === "Queen" || card.face === "King"){
+                card.value = 10
+            }
+            deck.push(card);
+        }
+    }
+}
+//---------------------->    SHUFFLE DECK
+const shuffleCards = ()  => {
+    while (deck.length > 0) {
+        let index = Math.floor(Math.random() * deck.length);
+        let card = deck[index];
+        shuffledDeck.push(card);
+        deck.splice(index, 1);
+    }
+}
+//---------------------->    DEAL HAND
+const dealHand = () => {
+    dealer.hand.push(shuffledDeck[0]);
+        shuffledDeck.splice(0, 1);
+    player.hand.push(shuffledDeck[0]);
+        shuffledDeck.splice(0, 1);
+    dealer.hand.push(shuffledDeck[0]);
+        shuffledDeck.splice(0, 1);
+    player.hand.push(shuffledDeck[0]);
+        shuffledDeck.splice(0, 1);
+}
+//---------------------->    HIT ME - deal another card  -->  HIT button
+const hitMe = () => {
+    let card = shuffledDeck.shift();
+    player.hand.unshift(card);
+}
+//---------------------->   PLAYER HAND VALUE  -->  STAY button
+
+const handValue = () => {
+    let playerCall = [];
+    for (let i = 0; i < player.hand.length; i++){
+        playerCall.push(player.hand[i].value);
+    }
+    for (let d = 0; d < dealer.hand.length; d++){
+        dealerCall.push(dealer.hand[d].value);
+        }
+    // const getSum = (total, num) => {return total + num;}
+        playerFinalScore = playerCall.reduce(getSum);
+        dealerFinalScore = dealerCall.reduce(getSum);
+    for (let i = 0; i < player.hand.length; i++){
+        if (player.hand[i].face == "Ace" && playerFinalScore > 21) {
+            playerFinalScore = playerFinalScore - 10;
+        } 
+    }
+}
+//---------------------->    AI - DEALER HIT  -->  STAY button
+const  dealerAI = () => {
+document.getElementById("dealerDown").className = `card ${dealer.hand[0].suit} r${dealer.hand[0].face}`;
+    while (dealerFinalScore < playerFinalScore && playerFinalScore <= 21) {
+        let card = shuffledDeck.shift();
+        dealer.hand.unshift(card);
+        dealerCall.unshift(card.value);
+        dealerFinalScore = dealerCall.reduce(getSum);
+        displayDealerCard();
+    }
+    for (let d = 0; d < dealer.hand.length; d++) {  //---------------------->    IDK WHY THIS WORKS YET BUT IT DOES?
+        if (dealer.hand[d].face == "Ace" && dealerFinalScore > 21) {
+            dealerFinalScore = dealerFinalScore - 10;
+            if (dealerFinalScore < playerFinalScore && playerFinalScore <= 21){
+                let card = shuffledDeck.shift();
+                dealer.hand.unshift(card);
+                dealerCall.unshift(card.value);
+                dealerFinalScore = dealerCall.reduce(getSum);
+                displayDealerCard();
+            }
+        }
+    }
+    console.log("                                               DEALER FINAL SCORE: " + dealerFinalScore)
+    console.log("PLAYER FINAL SCORE: " + playerFinalScore)
+}
+
+//---------------------->    PLAY HAND  -->  STAY button
+const callHand = () => {
+    if (playerFinalScore === 21) {
+        alert.innerText = "21!";
+    } if (dealerFinalScore > 21 && playerFinalScore <= 21) {
+        player.chips += 3;
+        if (playerFinalScore < 17 && dealerFinalScore > 21){
+        alert.innerText = "Dealer Bust!";
+        } else {
+            alert.innerText = "You Win!";
+        }
+    } else if (playerFinalScore <= 21 && playerFinalScore > dealerFinalScore) {
+        player.chips += 3;
+        console.log(alert)
+        alert.innerText = "You Win!";
+    } else if (playerFinalScore <= 21 && playerFinalScore === dealerFinalScore) {
+        player.chips++;
+        alert.innerText = "Its a tie.";
+    } else if (playerFinalScore < dealerFinalScore && dealerFinalScore <= 21) {
+        alert.innerText = "House Wins";  
+    } else if (playerFinalScore > 21) {
+        alert.innerText = "Bust";  
+    }
+    console.log("You now have: " +  player.chips + " chips")
+    
+    //-------->  anything else need to go in here?
+};
+//---------------------->       BUY IN
+const buyIn = () => {
+    if (player.chips === 0) {
+        console.log("click")
+        document.querySelector('#buy-button').style.visibility = "visible"
+    }   
+}
